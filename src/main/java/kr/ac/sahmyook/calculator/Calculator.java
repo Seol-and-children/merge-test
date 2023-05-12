30c8a714b8708eb270e73fafd2e7abd66a60f049
정건희 <wjdrjsgml08@naver.com>
2023-05-12 15:59:00 +0900

    더하기 프로그램 코드 개선

--- src/main/java/kr/ac/sahmyook/calculator/Calculator.java
+++ src/main/java/kr/ac/sahmyook/calculator/Calculator.java
@@ -3,17 +3,8 @@ package kr.ac.sahmyook.calculator;
 import java.util.Scanner;
 
 public class Calculator {
-    public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
-
-            System.out.print("첫 번째 정수를 입력하세요 : ");
-            int num1 = sc.nextInt();
-
-            System.out.print("두 번째 정수를 입력하세요 : ");
-            int num2 = sc.nextInt();
-
-            int sum = num1 + num2;
-
-            System.out.println("두 수의 합 : " + sum);
-        }
+    public int plus(int num1, int num2) {
+        System.out.println("두 수의 합 : " + plus(num1, num2));
+        return num1 + num2;
     }
+}
--- src/test/java/kr/ac/sahmyook/calculator/CalculatorTests.java
+++ src/test/java/kr/ac/sahmyook/calculator/CalculatorTests.java
@@ -1,23 +1,17 @@
-package kr.ac.sahmyook.calculator.test;
+package kr.ac.sahmyook.calculator;
 
 import kr.ac.sahmyook.calculator.Calculator;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
-
-import java.util.Scanner;
-import java.util.zip.Adler32;
 
 public class CalculatorTests {
 
     @DisplayName("더하기 프로그램")
     @Test
-    public void testAddition() {
-        int num1 = 1;
-        int num2 = 2;
-        int sum = num1 + num2;
-
+    public void testplus() {
+        Calculator calculator = new Calculator();
+        int sum = calculator.plus(1,2);
         Assertions.assertEquals(3, sum);
 
     }
